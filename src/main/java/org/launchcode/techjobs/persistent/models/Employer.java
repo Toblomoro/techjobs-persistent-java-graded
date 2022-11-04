f706beb0c24458d8975c7d6aeaf784cd4e535d0f
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-11-04 16:35:34 -0500

    Employer.java/skill worked out according to book

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,30 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required")
+    @Size(min = 3, max = 100, message = "Location must be between 3 and 100 characters")
+    public String location;
 
+    public void employer(){
+//        this.id = nextId;
+//        nextId++;
+//no- arg constructor
+    };
+
+
+
+    //Public access methods, Getters and Setters
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,20 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    public static String description;
+
+    public Skill() {
+    }
+
+    public static String getDescription() {
+        return description;
+    }
+
+    public static void setDescription(String description) {
+        Skill.description = description;
+    }
 }
