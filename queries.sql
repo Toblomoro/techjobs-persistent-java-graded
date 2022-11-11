97795c6ea49a69f5482d0bce9d83aaf847fa8822
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-11-11 11:56:44 -0600

    All tests and app working

--- queries.sql
+++ queries.sql
@@ -10,4 +10,7 @@ SELECT name FROM employer WHERE location = "St. Louis City";
 DROP TABLE job;
 
 -- Part 4: Test it with SQL
-
+SELECT * FROM skill
+INNER JOIN job_skills ON job_skills.skills_id = skill_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
--- src/main/java/org/launchcode/techjobs/persistent/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SearchController.java
@@ -29,7 +29,7 @@ public class SearchController {
 
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
-        Iterable<Job> jobs;
+       Iterable<Job> jobs;
         if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
             jobs = jobRepository.findAll();
         } else {
